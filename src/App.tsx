be91182584b1de34d0bab8a9efd6db4d5f7de317
claudiaGiancola <cloemezzaluna@gmail.com>
2024-07-30 14:01:26 +0100

    fetch and display picture of the day implemented + .env variable in progress
    Co-authored-by: Dorothy Wong <dorothyynwong@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Home from './Home/Home';
+// line below is just an example of what to import to use the DisplayPictureOfDay function
+// import { DisplayPictureOfDay } from './images/Fetch
 import { Quiz } from './Quiz/Quiz';
 import Footer from './Footer/Footer';
 
@@ -10,6 +12,8 @@ function App() {
     <div className="App">
       <Router>
         <h1>MarsioKart</h1>
+          {/* line below is just an example of what to import to use the DisplayPictureOfDay function */}
+          {/* <DisplayPictureOfDay /> */}
         <Routes>
           <Route path='/'
           element={<Home/>}/>
