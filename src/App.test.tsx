333fd033d9db7406fa50933106fbb4f74c22b758
anum1608 <riaz.anum@outlook.com>
2024-07-30 12:39:32 +0100

    MM-005: Add test for hamburger button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent  } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import Hamburger from './Hamburgerbutton';
 
-test('renders learn react link', () => {
+/* test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+}); */
+
+
+
+test('calls onClick when Hamburger button is clicked', () => {
+    const handleClick = jest.fn();
+    render(<Hamburger onClick={handleClick} label="Click Me" />);
+  
+    const button = screen.getByText('Click Me');
+    fireEvent.click(button);
+  
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
