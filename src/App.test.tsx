6034fea130e5df455631b8323e19006ce19bbb10
Natasha Buckham <natashaisobel@gmail.com>
2024-07-30 14:51:55 +0100

    Adds placeholder test to enable run build

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import "@testing-library/jest-dom";
+
+// remove this test when real tests are added
+test('renders header', () => {
+    render(<App />);
+    const header = screen.getByText(/MarsioKart/i);
+    expect(header).toBeInTheDocument();
+  });
--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <h1>MarsioKart</h1>
         <Routes>
           <Route path='/'
           element={<Home/>}/>
