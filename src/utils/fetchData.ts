bf16f1af1a05057a5a4aba750f491b1ee0275595
Dorothy Wong <dorothyynwong@gmail.com>
2024-07-31 11:41:08 +0100

    add comments on how to use fetchData with .env

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,3 +1,6 @@
+// 1. .env has to be created in root
+// 2. inside .env, it should be REACT_APP_NASA_API_KEY="<your api key>"
+
 export async function fetchAPI(apiUrl: string) {
     const apiKey = process.env.REACT_APP_NASA_API_KEY;
     const link = `${apiUrl}${apiKey}`;
