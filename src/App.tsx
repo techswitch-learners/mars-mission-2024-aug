af060aaef40202b66b2e35093c0e25156e9e8044
Dorothy Wong <dorothyynwong@gmail.com>
2024-07-31 14:01:31 +0100

    updated according to review 1. remove comments 2. add back .env.template 3.remove test data

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Home from './Home/Home';
-// line below is just an example of what to import to use the DisplayPictureOfDay function
-// import { DisplayPictureOfDay } from './images/Fetch
 import { Quiz } from './Quiz/Quiz';
 import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-
-      {/* line below is just an example of what to import to use the DisplayPictureOfDay function */}
-      {/* <DisplayPictureOfDay /> */}
-
       <Router>
         <h1>MarsioKart</h1>
-          {/* line below is just an example of what to import to use the DisplayPictureOfDay function */}
-          {/* <DisplayPictureOfDay /> */}
         <Routes>
           <Route path='/'
           element={<Home/>}/>
