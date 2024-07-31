cfdca4a734eb1e3df8dd223bbaedfaddb05187dd
Natasha Buckham <natashaisobel@gmail.com>
2024-07-31 12:12:32 +0100

    Creates questions file and writes questions as comments

---
+++ src/questions.tsx
@@ -0,0 +1,24 @@
+// 1. Where is Mars in the solar system?
+// a. Fourth planet from the sun
+// b. Next to Pluto
+// c. Closer to the sun than Earth
+
+// 2. What is a rover?
+// a. a dog
+// b. a robotic space vehicle
+// c. a type of car
+
+// 3. How many rovers have NASA sent to Mars?
+// a. 5
+// b. 4
+// c. 20
+
+// 4. Which of these lists names Mars rovers only?
+// a. Resilience, Curiosity, Perseverance
+// b. Sojourner, Oppertunity, Perseverance
+// c. Discovery, Searcher, Pioneer
+
+// 5. What are the Mars rovers looking for?
+// a. Ares God of War
+// b. Dead rovers
+// c. Water
