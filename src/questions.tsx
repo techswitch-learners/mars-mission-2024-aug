dea7b1bab8556eed4ec55a54a106256f2f5d5c5d
Natasha Buckham <natashaisobel@gmail.com>
2024-07-31 12:24:50 +0100

    Creates function which returns quiz questions in a data structure. Co-authored-by: Ruksan Emirali <RuksanEmirali@users.noreply.github.com>

--- src/questions.tsx
+++ src/questions.tsx
@@ -22,3 +22,14 @@
 // a. Ares God of War
 // b. Dead rovers
 // c. Water
+
+export function getQuizQuestions() {
+    const questions : Array<Object> = [
+        {"Where is Mars in the solar system?" : [{"Fourth planet from the sun" : true}, {"Next to Pluto" : false}, {"Closer to the sun than Earth" : false}]},
+        {"What is a rover?": [{"A dog" : false}, {"A robotic space vehicle" : true}, {"A type of car" : false}]},
+        {"How many rovers have NASA sent to Mars?": [{"5": true}, {"4": false}, {"20": false}]},
+        {"Which of these lists real Mars rover names?": [{"Resilience, Curiosity, Perseverance": false}, {"Sojourner, Oppertunity, Perseverance" : true}, {"Discovery, Searcher, Pioneer" : false}]},
+        {"What are the Mars rovers looking for?": [{"Ares God of War": false}, {"Dead rovers": false}, {"Water": true}]}
+    ]
+    return questions
+}
