a6da30a69cba9c873182192e2823b94a9bcbab71
Purbai <purbai.varsani@softwire.com>
2024-07-31 15:39:00 +0100

    MM-112-menuoptions: display menu items on/off when hamburger is clicked

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,8 @@ import { render, screen, fireEvent  } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 import "@testing-library/jest-dom";
-import Hamburger from './Hamburgerbutton';
+import Hamburger from './Header/Hamburgerbutton';
+import Menu from './Header/Menu';
 
 // remove this test when real tests are added
 test('renders header', () => {
@@ -12,13 +13,40 @@ test('renders header', () => {
     expect(header[0]).toBeInTheDocument();
   });
 
-test('calls onClick when Hamburger button is clicked', () => {
-    const handleClick = jest.fn();
-    render(<Hamburger onClick={handleClick} label="Click Me" />);
-  
-    const button = screen.getByText('Click Me');
-    fireEvent.click(button);
-  
-    expect(handleClick).toHaveBeenCalledTimes(1);
-  });
+// test('calls onClick when Hamburger button is clicked', () => {
+//     const handleClick = jest.fn();
+//     render(<Hamburger label="Click Me" />);
+//     const button = screen.getByText('Click Me');
+//     fireEvent.click(button);
+//     expect(handleClick).towHaveBeenCalledTimes(1);;
+//   });
+
+//   test('test for hamburger menu options ', () => {
+//     const handleClick = jest.fn();
+//     render(<Hamburger label="Click Me" />);
+//     const button = screen.getByText('Click Me');
+//     fireEvent.click(button);
+//     const menuitem = screen.getByText('Your Acheivements');
+//     expect(menuitem).toBeInTheDocument();
+//   });
+
+test('should toggle menu on button click', () => {
+  render(<Hamburger />);
+
+  const button = screen.getByTestId('toggle-button');
+  const menu = screen.queryByTestId('data-testid');
+
+  // Initially, the menu should not be visible
+  expect(menu).not.toBeInTheDocument();
+
+  // Click the button to open the menu
+  fireEvent.click(button);
+  expect(screen.getByTestId('menu')).toBeInTheDocument();
+
+  // Click the button again to close the menu
+  fireEvent.click(button);
+  expect(screen.queryByTestId('menu')).not.toBeInTheDocument();
+});
+
+
 
