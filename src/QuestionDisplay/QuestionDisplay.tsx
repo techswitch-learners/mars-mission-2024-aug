4391c7e3955aef23514c54ca0351ce145da0b40f
Amy <amysun.uk@gmail.com>
2024-08-01 10:27:26 +0100

    display questions wip

--- src/QuestionDisplay/QuestionDisplay.tsx
+++ src/QuestionDisplay/QuestionDisplay.tsx
@@ -6,9 +6,12 @@ export function QuestionDisplay() {
     const question = questions[Math.floor(Math.random() * questions.length)];
 
     return (
-        <>
-            
-        </>
+        <div>
+            {question.question}
+            <input type="radio" id="radio-button" /> {question.answers[0].answer}
+            <input type="radio" id="radio-button" /> {question.answers[1].answer}
+            <input type="radio" id="radio-button" /> {question.answers[2].answer}
+        </div>
 
     )
 }
