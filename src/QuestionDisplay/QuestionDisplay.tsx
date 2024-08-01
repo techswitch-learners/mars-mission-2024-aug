65a01e9ae345c3a17b9dd0741aa653eaf52a3557
Amy <amysun.uk@gmail.com>
2024-08-01 16:14:42 +0100

    removed comments

--- src/QuestionDisplay/QuestionDisplay.tsx
+++ src/QuestionDisplay/QuestionDisplay.tsx
@@ -6,13 +6,6 @@ interface QuestionDisplayProp{
 }
 
 export function QuestionDisplay(props: QuestionDisplayProp) {
-    // const questions = getQuizQuestions();
-
-    // const question = questions[Math.floor(Math.random() * questions.length)];
-
-    // function updateCurrentIndex() {
-    //     setCurrentIndex(currentIndex + 1);
-    // }
 
     return (
         <div>
@@ -20,7 +13,6 @@ export function QuestionDisplay(props: QuestionDisplayProp) {
             <input type="radio" name="answer" id="radio-button1" value="0" /> {props.question.answers[0].answer} <br/>
             <input type="radio" name="answer" id="radio-button2" value="1" /> {props.question.answers[1].answer} <br/>
             <input type="radio" name="answer" id="radio-button3" value="2" /> {props.question.answers[2].answer} <br/>
-            {/* <button type="submit" className="submit-answer" onClick={() => updateCurrentIndex() }>Submit</button> */}
         </div>
 
     )
