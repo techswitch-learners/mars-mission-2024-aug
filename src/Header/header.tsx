ea0375550ab04a108fb386d9c614c41903c410e2
Kiran Randhawa Kukar <kiran.randhawakukar@softwire.com>
2024-08-01 12:39:51 +0100

    MM.112: fix gheader capitalization v3

--- src/Header/header.tsx
+++ src/Header/header.tsx
@@ -4,9 +4,13 @@ import Hamburger from '../Hamburgerbutton/Hamburgerbutton';
 const logo = require('../ImageAssets/marsiokartlogo.jpg')
 
 const Header: React.FC = () => {
+  const handleHamburgerClick = () => {
+    console.log('Hamburger menu clicked!');
+  };
+
   return (
     <div className="App">
-      <Hamburger />
+      <Hamburger onClick={handleHamburgerClick} label={""}/>
       <img src={logo} alt="Marsio Kart Logo" />
     </div>
   );
