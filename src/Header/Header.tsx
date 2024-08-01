420ab18cb94e3bb6f426eb3a96f2c06748b336a8
ruksan_emirali <ruksanemirali@gmail.com>
2024-08-01 15:08:35 +0100

    Resolves merge conflicts

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Hamburger from '../Hamburgerbutton/Hamburgerbutton';
+import './Header.scss'
 
 const logo = require('../ImageAssets/marsiokartlogo.jpg')
 
 const Header: React.FC = () => {
 
   return (
-    <div className="App">
+    <div className="header">
       <Hamburger/>
       <img src={logo} alt="Marsio Kart Logo" />
     </div>
