973a694b92224d3dbb30fef23a5745ca0b2b5513
Purbai <purbai.varsani@softwire.com>
2024-08-01 12:04:47 +0100

    mm-112-menuoptions: updated the import header

--- src/Header/header.tsx
+++ src/Header/header.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Hamburger from '../Hamburgerbutton/Hamburgerbutton';
 
+const logo = require('../ImageAssets/marsiokartlogo.jpg')
+
 const Header: React.FC = () => {
+  const handleHamburgerClick = () => {
+    console.log('Hamburger menu clicked!');
+  };
+
   return (
     <div className="App">
-      {/* call to display the hamburger button and menu items */}
-      <Hamburger />
+      <Hamburger onClick={handleHamburgerClick} label={""}/>
+      <img src={logo} alt="Marsio Kart Logo" />
     </div>
   );
 };
