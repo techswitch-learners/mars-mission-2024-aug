533e5e5e467a1625d40cb0ab3944451899228f80
Dorothy Wong <dorothyynwong@gmail.com>
2024-08-02 10:27:59 +0100

    remove useEffect which is never used

--- src/images/PreviewImage.tsx
+++ src/images/PreviewImage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { PictureOfDay } from "./DisplayPicture";
 import { fetchAPI, fetchRandomAPI } from "../utils/fetchData";
 
